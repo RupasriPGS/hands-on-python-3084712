caba8799bc323923f8ed9be09d63d5dc22177d20
RupasriPGS <159116272+RupasriPGS@users.noreply.github.com>
2026-02-23 21:25:13 +0000

    02_01_begin

--- Ex_Files/02_01_begin/main.py
+++ Ex_Files/02_01_begin/main.py
@@ -1,13 +1,18 @@
-RUN_INDENTED = False
+def my_function():
+    greet = "Hello"
+    return greet
+
+RUN_INDENTED = True
 
 message = "running unindented"
 
 if RUN_INDENTED:
+    print(my_function())
     message = "running indented"
+    #my_function()
 
 print(message)
 
 
-def my_function():
-    greet = "Hello"
-    return greet
+
+
