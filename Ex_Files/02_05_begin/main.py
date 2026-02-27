f49ce33c97c04342d1df5878246c6e1d66a88260
RupasriPGS <159116272+RupasriPGS@users.noreply.github.com>
2026-02-27 20:51:34 +0000

    02_05_begin

--- Ex_Files/02_05_begin/main.py
+++ Ex_Files/02_05_begin/main.py
@@ -10,11 +10,11 @@ current_env = os.environ.get("ENV_NAME", DEVELOPMENT)
 
 if current_env == DEVELOPMENT:
     print("Development environment")
-elif current_env == PRODUCTION:
-    print("Production environment")
+#elif current_env == PRODUCTION:
+#    print("Production environment")
 elif current_env == STAGING:
     print("Staging environment")
-elif current_env in [CODE_SPACE, LOCAL]:
-    print("Codespace or local environment")
+elif current_env in [CODE_SPACE, LOCAL, PRODUCTION]:
+    print("Codespace or local or production environment")
 else:
     print("Unknown environment")
