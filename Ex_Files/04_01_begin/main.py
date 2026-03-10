8550778d9f97b79c144fd3427a320b1f1452a998
RupasriPGS <159116272+RupasriPGS@users.noreply.github.com>
2026-03-10 19:59:37 +0000

    04_01_begin

--- Ex_Files/04_01_begin/main.py
+++ Ex_Files/04_01_begin/main.py
@@ -4,5 +4,8 @@ response = requests.get(
     "http://api.worldbank.org/v2/countries/USA/indicators/SP.POP.TOTL?per_page=5000&format=json",
 )
 
-last_twenty_years = response.json()[1][:20]
+last_twenty_years = response.json()[1][:30]
+
+for year in last_twenty_years:
+    print(year["date"], year["value"])
 
